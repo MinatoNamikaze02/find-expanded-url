bfc4a744dafccc5e7bb749afc648ad30e3f15e90
Arjun S <arjuns@Arjuns-MacBook-Air.local>
2022-08-23 21:59:47 +0530

    minor fixes

--- find_expanded_url/url_expander.js
+++ find_expanded_url/url_expander.js
@@ -17,28 +17,4 @@ async function postData({url, error}) {
   }
 }
 
-// const getArgs = () => {
-//   const args = process.argv.slice(2)
-//   if(args.length == 0 || args.length > 2) {
-//     console.log("No arguments provided or too many arguments provided")
-//     process.exit(0)
-//   }
-//   var url = args[0]
-//   var error = args[1]
-//   return {
-//     url: url,
-//     error: error ? error : "false"
-//   }
-// }
-
-// postData(getArgs()).then((data) => {
-//   var url = data.headers.get('location') === null ? data.url : data.headers.get('location')
-//   var server = data.headers.get('server')
-//   console.log("The actual URL is: ", url)
-//   console.log("The server is: ", server)
-// }).catch(err => {
-//   console.log(err.message)
-//   process.exit(0)
-// })
-
 export default postData
